8636840c8abb6478c87bed31b4d4924ac98a6789
Amelie Schreiber <thesingularity.research@gmail.com>
2020-06-24 14:10:07 -0700

    name change
    changed name

--- SurfaceCodes/__init__.py
+++ SurfaceCodes/__init__.py
@@ -1,4 +1,4 @@
-from SurfaceCodes.surface_code_class import SurfaceCodeGraph
+from SurfaceCodes.surface_code_graph import SurfaceCodeGraph
 from SurfaceCodes.surface_code_circuit import SurfaceCodeCircuit
 from .utilites import permlist_to_tuple
 __all__ = ["SurfaceCodeGraph", "SurfaceCodeCircuit", "permlist_to_tuple"]
